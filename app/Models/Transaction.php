192f2abdabad8464053664ea7f51a9ed7bc3e94a
Dwi Andika F <dwiezy1509@gmail.com>
2025-03-07 20:25:03 +0700

    model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,27 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-        //
-        protected $fillable = ['user_id', 'product_id', 'quantity', 'total_price', 'status', 'payment', 'transfer_poto'];
+    //
+    protected $fillable = ['order_id', 'user_id', 'product_id', 'quantity', 'total_price', 'status', 'payment', 'transfer_poto'];
 
-        public function user()
-        {
-            return $this->belongsTo(User::class);
-        }
-        public function product()
-        {
-            return $this->belongsTo(Product::class);
-        }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+
 
     protected static function boot()
     {
         parent::boot();
 
         static::creating(function ($transaction) {
+            // Generate order_id jika belum ada
+            if (!$transaction->order_id) {
+                $transaction->order_id = 'ORD-' . Str::upper(Str::random(10));
+            }
+
+            // Hitung total_price jika belum ada
             if (!$transaction->total_price) {
                 $product = Product::find($transaction->product_id);
                 $transaction->total_price = $product ? $product->price * $transaction->quantity : 0;
