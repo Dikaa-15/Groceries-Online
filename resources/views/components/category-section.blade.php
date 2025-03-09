c46fa729356fe72bb89d4e5c8b5843e8394b5e02
Dwi Andika F <dwiezy1509@gmail.com>
2025-03-09 19:37:58 +0700

    fixed blade

--- resources/views/components/category-section.blade.php
+++ resources/views/components/category-section.blade.php
@@ -1,4 +1,4 @@
-<div class="container mx-auto px-6 py-16">
+<div class="container mx-auto px-6 py-10">
     <h2 class="text-4xl font-bold text-gray-800 text-center mb-10">Shop by Category</h2>
 
     <!-- Wrapper Scroll (Hanya aktif di Mobile) -->
